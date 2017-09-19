f03adf38bd27433ed0e6cc84d767061c524a993c
yasong <752198313@qq.com>
2017-09-19 23:04:24 +0800

    add python file

---
+++ BinDiff_in_Python/BinDiff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Xiaokang Yin
+# @Date:   2017-09-19 22:44:49
+# @Last Modified by:   Xiaokang Yin
+# @Last Modified time: 2017-09-19 23:04:05
+
+
+#This program implemnts finding the differences between binary files.
+#The first step is try to recode the c bindiff program with python
+#then maybe change something to make it work better.
+#
+
+import sys
+import math
+
+
+
+#############
+#
