bbe3d263299a5e12bb130fd5851f665475ef9852
yasong <752198313@qq.com>
2017-09-20 08:21:38 +0800

    add usage()

--- BinDiff_in_Python/BinDiff.py
+++ BinDiff_in_Python/BinDiff.py
@@ -3,7 +3,7 @@
 # @Author: Xiaokang Yin
 # @Date:   2017-09-19 22:44:49
 # @Last Modified by:   Xiaokang Yin
-# @Last Modified time: 2017-09-19 23:04:05
+# @Last Modified time: 2017-09-20 08:16:07
 
 
 #This program implemnts finding the differences between binary files.
@@ -18,3 +18,23 @@ import math
 
 #############
 #
+def usage():
+    print "Binary Difference Tool"
+    print
+    print "Usage: BinDiff.py [-hcdstxC] [-b B] [-n N] [-f F] [-g G] [-y Y] file1 file2"
+    print "    -h: show usage information and exit."
+    print "    -c: concise output positions in decimal instead of hex."
+    print "    -d: output byte positions in decimal instead of hex."
+    print "    -s: stop after first block of differing bytes."
+    print "    -t: print total number of bytes in differing blocks."
+    print "    -x: output lenghts in hex instead of decimal."
+    print "    -C: use capitals for hex values."
+    print "    -b B: compare per block of B bytes."
+    print "    -n N: treat N identical bytes or blocks in between two differing bytes/blocks."
+    print "          as different, i.e. treat the whole as the same chunk."
+    print "    -f F: dump each differing block to a file with as name byte position prefixed"
+    print "          with F."
+    print "    -g G: dump each original block to a file with as name byte position prefixed"
+    print "          with G."
+    print "    -y Y: compare file1 with a virtual file of same length, filled with bytes Y."
+    print "          Y must be an integer between 0 and 255 (hex allowed if prefixed with 0x)."
